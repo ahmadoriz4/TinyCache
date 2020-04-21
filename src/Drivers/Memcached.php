b8effdc5b7beb3bd9ff45f79988f4bf87eb8aaab
oriza <ahmadoriza@gmail.com>
2020-04-21 10:14:49 +0700

    Build driver for Memcached

--- src/Drivers/Memcached.php
+++ src/Drivers/Memcached.php
@@ -3,6 +3,10 @@
 /**
  * Simple Driver for Memcached.
  *
+ * This class just wrap the Memcached PHP Ext and follow PSR 16 Interface.
+ * We don't extend Memcached in this class. Just inject the class in construct.
+ * Maybe in the future we change `php-memcached` to other Ext.
+ * 
  * @package Drivers
  * @author Gemblue
  */
@@ -10,20 +14,25 @@
 namespace Gemblue\TinyCache;
 
 class Memcached implements CacheInterface
-{    
+{
     /** Ext container */
     protected $memcached;
     
     /**
      * Constructor 
      * 
-     * Handle connection and Inject Ext.
+     * Handle connection and inject ext.
      * 
      * @return void
      */
-    public function __construct(string $host, int $port, int $timeout)
+    public function __construct(string $host, int $port, bool $persistence)
     {       
-       
+        // Inject dependency.
+        $this->memcached = new \Memcached;
+        
+        if (!$this->memcached->addServer($host, $port, $persistence)) {
+            return new Exception('Failed to connect, maybe Memcached server is not running, or wrong config for host and port.');
+        }
     }
     
     /**
@@ -33,7 +42,12 @@ class Memcached implements CacheInterface
      */
     public function get(string $key, $default = null) 
     {
+        $get = $this->memcached->get($key);
         
+        if ($get == false)
+            return $default;
+
+        return $get;
     }
 
     /**
@@ -43,7 +57,7 @@ class Memcached implements CacheInterface
      */
     public function set(string $key, string $value, int $ttl = null) 
     {
-        
+        return $this->memcached->set($key, $value, $ttl);
     }
     
     /**
@@ -53,7 +67,7 @@ class Memcached implements CacheInterface
      */
     public function delete(string $key) 
     {
-        
+        return $this->memcached->delete($key) ?? false;
     }
 
     /**
@@ -63,7 +77,7 @@ class Memcached implements CacheInterface
      */
     public function clear() 
     {
-        
+        return $this->memcached->flush();
     }
 
     /**
@@ -73,7 +87,13 @@ class Memcached implements CacheInterface
      */
     public function getMultiple(array $keys, $default = null) 
     {
-       
+        $temp = [];
+
+        foreach ($keys as $key) {
+            $temp[] = [$key => $this->memcached->get($key)];
+        }
+        
+        return $temp ?? $default;
     }
 
     /**
@@ -83,7 +103,12 @@ class Memcached implements CacheInterface
      */
     public function setMultiple(iterable $values, int $ttl = null) 
     {
-        
+        foreach ($values as $key => $value) {
+            if (!$this->memcached->set($key, $value, $ttl))
+                return false;
+        }
+
+        return true;
     }
 
     /**
@@ -93,7 +118,12 @@ class Memcached implements CacheInterface
      */
     public function deleteMultiple(array $keys) 
     {
-       
+        foreach ($keys as $key) {
+            if (!$this->memcached->delete($key))
+                return false;
+        }
+
+        return true;
     }
 
     /**
@@ -105,6 +135,9 @@ class Memcached implements CacheInterface
      */
     public function has(string $key) 
     {
-        
+        if ($this->memcached->get($key))
+            return true;
+
+        return false;
     }
 }
